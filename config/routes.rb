cf0070c1fac0b15b008f24820aef649a456e1a90
Graciella Dharmawan <g.dharmawan@student.maastrichtuniversity.nl>
2021-05-25 15:39:58 +0200

    solve 2-rails task manager

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :tasks
+
+  # # show all tasks on the homepage/index
+  # get 'tasks', to: 'tasks#index'
+
+  # # create a new task
+  # get 'tasks/new', to: 'tasks#new', as: :new_task
+
+  # # show a specific task's details
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+  # post 'tasks', to: 'tasks#create'
+
+  # # update a task (get the form, patch the parameters to update)
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch 'tasks/:id', to: 'tasks#update'
+
+  # # delete a task
+  # delete 'tasks/:id', to: 'tasks#destroy'
 end
